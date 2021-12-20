475c3a31b1d9f1fba5a401ae4976798017eec6db
SaoLouis <noiret.louis17@gmail.com>
2021-12-20 14:39:31 +0100

    "Ajout de ma formation de Python sur Udemy"

--- cours/class_method.py
+++ cours/class_method.py
@@ -1,6 +1,25 @@
 class Voiture:
+	voiture_crees = 0
 	def __init__(self, marque, vitesse, prix):
+		Voiture.voiture_crees += 1
 		self.marque = marque
 		self.vitesse = vitesse
 		self.prix = prix
 
+#Méthodes de classe
+	@classmethod
+	def Lamborghini(cls):
+		return cls(marque="Lamborghini",vitesse=200, prix=200000)
+
+	@classmethod
+	def Porsche(cls):
+		return cls(marque="Lamborghini", vitesse=200, prix=180000)
+
+#Méthode statique
+	@staticmethod
+	def afficher_nombre_voitures():
+		print(f"Vous avez {Voiture.voiture_crees} voitures dans votres garages.")
+
+lamborghini = Voiture.Lamborghini()
+porsche = Voiture.Porsche()
+Voiture.afficher_nombre_voitures()
